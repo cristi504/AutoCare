4dd608bed66c9bbb764bc7f18580c16e56f5208e
AlmLeonardo <leoalmajanu01@gmail.com>
2024-11-17 21:25:42 +0200

    Pozele

---
+++ autocare/src/LeftPhoto.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const LeftPhoto = () => {
+  return (
+    <div className="page-container">
+      <h1>Documente Necesare</h1>
+      <p>This page is under construction.</p>
+    </div>
+  );
+};
+
+export default LeftPhoto;
---
+++ autocare/src/RightPhoto.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const RightPhoto = () => {
+  return (
+    <div className="page-container">
+      <h1>Martorii din Bord</h1>
+      <p>This page is under construction.</p>
+    </div>
+  );
+};
+
+export default RightPhoto;
